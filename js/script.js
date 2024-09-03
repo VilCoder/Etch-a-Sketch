90e6e78f7f3657fd835ea2f6f600b27baed9b81e
VilCoder <176792549+VilCoder@users.noreply.github.com>
2024-09-02 23:25:22 -0500

    Add modal to set the number of grid cells
    This commit introduces and interactive modal to allows user to define
    the number of cells in the grid.
    
    key changes:
    - Added a modal in the HTML that is triggered when clicking the
    "Configure Grid" button.
    - Update css to style the modal, ensuring it is responsive and centered
    on the screen.
    - Implemented JavaScript logic to open and close the modal, as well as
    capture the value entered by the user and regenerate the grid based on
    the new settings.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 function setGrid(itemCount) {
-    const gridItemWidth = Math.floor(gridContainer.clientWidth / itemCount);
-    alert(gridItemWidth);
     
+    if (itemCount < 1 || itemCount > 100) {
+        itemCount = 16;
+    }
+
+    const gridItemWidth = Math.floor(gridContainer.clientWidth / itemCount);
+
     for (let i = 1; i <= itemCount; i++) {
         for (let j = 1; j <= itemCount; j++) {
             const gridItem = document.createElement("div");
@@ -20,5 +24,44 @@ function setGrid(itemCount) {
 }
 
 
-const gridContainer = document.querySelector(".grid-container");
+const gridContainer = document.querySelector(".grid");
 setGrid(16);
+
+const openModal = document.querySelectorAll("[data-open]");
+const closeModal = document.querySelectorAll("[data-close]");
+const isVisible = "is-visible";
+
+openModal.forEach(modal => {
+    modal.addEventListener("click", function() {
+        const modalId = this.dataset.open;
+        document.getElementById(modalId).classList.add(isVisible);
+    });
+});
+
+
+closeModal.forEach(modal => {
+    modal.addEventListener("click", function() {
+       this.parentElement.parentElement.parentElement.classList.remove(isVisible); 
+    });
+});
+
+document.addEventListener("click", e => {
+    if (e.target === document.querySelector(".modal.is-visible")) {
+        document.querySelector(".modal.is-visible").classList.remove(isVisible);
+    }
+});
+
+document.addEventListener("keyup", e => {
+    if (e.key === "Escape" && document.querySelector(".modal.is-visible")) {
+        document.querySelector(".modal.is-visible").classList.remove(isVisible);
+    }
+});
+
+const btnSend = document.querySelector(".btn-send");
+const input = document.querySelector(".value");
+btnSend.addEventListener("click", () => {
+    const itemCount = Number(input.value);
+    input.value = "";
+    gridContainer.textContent = "";
+    setGrid(itemCount);
+});
